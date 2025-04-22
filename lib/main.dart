2a3ab69dd666489bc4f3b54592486619944c99c5
Sajad54 <sajadba@gmail.com>
2025-04-22 12:51:41 -0400

    Security Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // NEW
 import 'package:urban_treasure/views/screens/auth/splash_page.dart';
 import 'package:urban_treasure/models/themes_notifier.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Load environment variables from .env
+  await dotenv.load();
+
   await Supabase.initialize(
-    url: "https://vtsdaeaxlcwwbgdpvjrd.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0c2RhZWF4bGN3d2JnZHB2anJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExOTY0OTMsImV4cCI6MjA1Njc3MjQ5M30.ID5oY0A9mp-bXB7pOWwQdloX78C9h90C8l9bKXwjGeA",
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(
@@ -33,7 +38,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       themeMode: themeNotifier.themeMode,
-      home: const SplashPage(), // SplashPage handles loading + redirects
+      home: const SplashPage(),
     );
   }
 }
